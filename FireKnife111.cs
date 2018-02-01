c02b826c83090cb2469cd92b6822e0762ddea446
times69 <gaiaqin@sina.cn>
2018-02-01 10:00:58 +0800

    Revert "瑞特"
    This reverts commit 0ec79c7f09c8c374bfc1f423ce7a455086db6bc5.

--- FireKnife111.cs
+++ FireKnife111.cs
@@ -30,10 +30,6 @@ public class FireKnife : MonoBehaviour
 
 
         tempPosition = new Vector3(0, 1.0f, 0);
-        tempPosition = new Vector3(0, 1.0f, 0);
-        tempPosition = new Vector3(0, 1.0f, 0);
-        tempPosition = new Vector3(0, 1.0f, 0);
-        tempPosition = new Vector3(0, 1.0f, 0);
     }
     // Use this for initialization
     void Start()
