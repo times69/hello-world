f8265e096ff3aba525ab1dca9054e931657755e0
times69 <gaiaqin@sina.cn>
2018-02-01 10:57:17 +0800

    Revert "适当放松的"
    This reverts commit 254c98980188a46eaf469f08ce3b2d7d6914d5d6.

--- FireKnife111.cs
+++ FireKnife111.cs
@@ -32,10 +32,12 @@ public class FireKnife : MonoBehaviour
         tempPosition = new Vector3(0, 1.0f, 0);
         tempPosition = new Vector3(0, 1.0f, 0);
         //tempPosition = new Vector3(0, 1.0f, 0);
-
+        tempPosition = new Vector3(0, 1.0f, 0);
+        tempPosition = new Vector3(0, 1.0f, 0);
 
         spriteRender.sortingLayerName = "kniftLayer";
         spriteRender.sortingOrder = 2;
+
 
 
         phyMaterial = bCollider2D.sharedMaterial;
